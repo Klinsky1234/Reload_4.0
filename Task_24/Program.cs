e5675b9ba53159a6eee8bfc2760a46cae4404bfe
Dmitriy Kalininskiy <kalininskiyds@gmail.com>
2023-02-11 15:14:39 +0300

    24

--- Task_24/Program.cs
+++ Task_24/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 24. Напишите прогу, которая принимает на вход число (А) и выдаёт сумму от 1 до А
+7 -> 28
+4 -> 10 */
+int a;
+Console.WriteLine($"Введите число А: ");
+int.TryParse(Console.ReadLine()!, out a);
+int sum = 0;
+for (int i = 1; i <= a; i++)
+{
+    sum = sum + i;
+}
+Console.WriteLine($" Сумма от 1 до А: {sum}");
